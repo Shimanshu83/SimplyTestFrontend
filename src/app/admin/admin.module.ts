827b55a9216dea2689b97b42bde3395c74042b2f
Shimanshu Yadav <shimanshu.yadav@vernost.in>
2023-04-15 18:41:43 +0530

    added basic question config

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -10,6 +10,11 @@ import { TestBasicSettingComponent } from './test-configuration/test-basic-setti
 import { TestQuestionManagerComponent } from './test-configuration/test-question-manager/test-question-manager.component';
 import { TestTimeAccessSettingComponent } from './test-configuration/test-time-access-setting/test-time-access-setting.component';
 import { TestResultTableComponent } from './test-configuration/test-result-table/test-result-table.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgxEditorModule } from "ngx-editor";
+
 const routes : Routes = [
   {path : "admin" , component : TestListingComponent},
   { path : "admin/:test" , component : TestConfigurationComponent 
@@ -36,7 +41,12 @@ const routes : Routes = [
   ],
   imports: [
     RouterModule.forChild(routes),
-    CommonModule
+    CommonModule,
+    ReactiveFormsModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    BrowserModule,
+    NgxEditorModule
   ],
   providers :[
     AdminService
--- src/app/admin/test-configuration/test-basic-setting/test-basic-setting.component.ts
+++ src/app/admin/test-configuration/test-basic-setting/test-basic-setting.component.ts
@@ -1,10 +1,52 @@
-import { Component } from '@angular/core';
-
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import {  Editor, Toolbar } from 'ngx-editor';
 @Component({
   selector: 'app-test-basic-setting',
   templateUrl: './test-basic-setting.component.html',
   styleUrls: ['./test-basic-setting.component.scss']
 })
-export class TestBasicSettingComponent {
+export class TestBasicSettingComponent implements OnInit, OnDestroy {
 
+  testForm: FormGroup;
+  
+  editor: Editor;
+  toolbar: Toolbar = [
+    ['bold', 'italic'],
+    ['underline', 'strike'],
+    ['code', 'blockquote'],
+    ['ordered_list', 'bullet_list'],
+    [{ heading: ['h1', 'h2', 'h3', 'h4', 'h5', 'h6'] }],
+    ['link', 'image'],
+    ['text_color', 'background_color'],
+    ['align_left', 'align_center', 'align_right', 'align_justify'],
+  ];
+
+
+
+  constructor(private fb: FormBuilder) { }
+
+  ngOnInit(): void {
+
+    this.createForm();
+    this.editor = new Editor();
+  }
+
+  createForm() {
+    this.testForm = this.fb.group({
+      testName: ['', Validators.required],
+      testCode: ['', Validators.required],
+      description: ['', Validators.required],
+      termsCondition: ['', Validators.required]
+    });
+  }
+
+  onSubmit() {
+    console.log(this.testForm.value);
+  }
+
+
+  ngOnDestroy(): void {
+    this.editor.destroy();
+  }
 }
